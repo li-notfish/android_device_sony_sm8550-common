29f095c7d6a871b8845c54453f01f5777088347e
someone5678 <59456192+someone5678@users.noreply.github.com>
2025-05-27 17:18:25 +0900

    sm8550-common: Build vendor.egistec.hardware.fingerprint@4.0
    * Matches stock
    
    Change-Id: Iaa151d2680613f612abe3366e5696b74a1f3f15a

--- common.mk
+++ common.mk
@@ -172,7 +172,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint-service.lineage
+    android.hardware.biometrics.fingerprint-service.lineage \
+    vendor.egistec.hardware.fingerprint@4.0.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
