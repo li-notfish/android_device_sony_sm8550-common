61c188e2dc2a15a49be31cda5056442d894f3495
onlyfish <linotfish@outlook.com>
2025-06-26 14:08:41 +0800

    sm8550-common: Fix removepackage file

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -1,16 +1,19 @@
 LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
+
 #vendor/gms
 LOCAL_OVERRIDES_PACKAGES += \
-    CalendarGoogle \
+    CalendarGooglePrebuilt \
     Maps \
     YouTube \
     Drive \
-    PersonalSafety \
-    PrebuiltGmail
+    SafetyHubSuwPrebuilt \
+	PrebuiltGmail
+
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
