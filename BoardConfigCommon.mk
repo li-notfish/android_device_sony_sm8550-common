57d68c5e500862755f365f9515fb168005f6162a
onlyfish <linotfish@outlook.com>
2025-11-04 17:42:29 +0800

    sm8550-common: ify to pixelos

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -162,8 +162,8 @@ TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    hardware/sony/vintf/device_framework_matrix.xml \
-    vendor/aosp/config/device_framework_matrix.xml
+    hardware/sony/vintf/device_framework_matrix.xml
+    
 DEVICE_FRAMEWORK_MANIFEST_FILE += $(COMMON_PATH)/framework_manifest.xml
 DEVICE_MATRIX_FILE += hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += \
