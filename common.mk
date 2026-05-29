3110b400f6f2cdb71bac5c5bb2ec9711c358aaf8
onlyfish <linotfish@outlook.com>
2026-05-29 21:14:42 +0800

    sm8550-common: powerhint: copy prebuilt task_profiles.json and modify Actions values
    revert "sm8550-common: tune powerhint.json"
    
    this revert a commit part f33fb5ff3eecc55782cd8d37e3b281505b716e7a
    
    sm8550-common: powerhint: add INTERACTIVE node

--- common.mk
+++ common.mk
@@ -142,7 +142,7 @@ PRODUCT_COPY_FILES += \
 # Cgroup & task_profiles
 PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
-    system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
 # Dalvik
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
