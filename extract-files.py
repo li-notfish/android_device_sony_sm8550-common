0f96afe7a2ea7355d3db62b30d3e01040d87f849
Luofan Chen <clfbbn@gmail.com>
2026-05-30 23:31:52 +0800

    sm8550-common: Use libcrypto-v33 for nv_updater
    As of commit 05780b7c of external/boringssl, the `int type` field in
    `struct evp_pkey_st` is removed. This causes nv_updater unable to
    determine the encrypted key type when decrypting the nv related xml
    files.
    
    Make it libcrypto-v33 so that it can successfully decrypt the files and
    update NV on boot.
    
    Change-Id: Id106fd035414720fd43be142ea86268d7ed035db

--- extract-files.py
+++ extract-files.py
@@ -137,6 +137,8 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
     'system_ext/lib64/vendor.qti.hardware.qccsyshal@1.2-halimpl.so': blob_fixup()
         .replace_needed('libprotobuf-cpp-full.so', 'libprotobuf-cpp-full-21.7.so'),
+    'vendor/bin/nv_updater': blob_fixup()
+        .replace_needed('libcrypto.so', 'libcrypto-v33.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
